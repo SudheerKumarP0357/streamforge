7d509e6deeb65506657113b2a4c86a2ba5bcc560
Sudheer Kumar P <sudheerkumarp0357@gmail.com>
2026-05-29 21:01:12 +0530

    update: syncphases for argocd [skip ci]

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,47 +1,47 @@
-resource "azurerm_key_vault" "main" {
-  name                        = "kv-${local.name}"
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  enabled_for_disk_encryption = false
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+# resource "azurerm_key_vault" "main" {
+#   name                        = "kv-${local.name}"
+#   location                    = azurerm_resource_group.main.location
+#   resource_group_name         = azurerm_resource_group.main.name
+#   enabled_for_disk_encryption = false
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = false
 
-  rbac_authorization_enabled = true
+#   rbac_authorization_enabled = true
 
-  public_network_access_enabled = false
+#   public_network_access_enabled = false
 
-  sku_name = "standard"
+#   sku_name = "standard"
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
 
 
-resource "azurerm_role_assignment" "key_vault_admin_terraform" {
-  principal_id         = data.azurerm_client_config.current.object_id
-  scope                = azurerm_key_vault.main.id
-  principal_type       = "ServicePrincipal"
-  role_definition_name = "Key Vault Administrator"
-}
+# resource "azurerm_role_assignment" "key_vault_admin_terraform" {
+#   principal_id         = data.azurerm_client_config.current.object_id
+#   scope                = azurerm_key_vault.main.id
+#   principal_type       = "ServicePrincipal"
+#   role_definition_name = "Key Vault Administrator"
+# }
 
-resource "azurerm_role_assignment" "key_vault_admin" {
-  principal_id         = var.key_vault_admin_object_id
-  scope                = azurerm_key_vault.main.id
-  role_definition_name = "Key Vault Administrator"
-  principal_type       = "User"
-}
+# resource "azurerm_role_assignment" "key_vault_admin" {
+#   principal_id         = var.key_vault_admin_object_id
+#   scope                = azurerm_key_vault.main.id
+#   role_definition_name = "Key Vault Administrator"
+#   principal_type       = "User"
+# }
 
-module "keyvault" {
-  source                = "./modules/private-endpoint"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  subnet_id             = azurerm_subnet.keyvault_subnet.id
-  private_endpoint_name = "keyvault"
-  resource_id           = azurerm_key_vault.main.id
-  subresource_names     = ["Vault"]
-  tags                  = var.tags
-  private_dns_zone_name = "privatelink.vaultcore.azure.net"
-  jump_vnet_id          = azurerm_virtual_network.jump_vnet.id
-  app_vnet_id           = azurerm_virtual_network.app_vnet.id
-}
+# module "keyvault" {
+#   source                = "./modules/private-endpoint"
+#   resource_group_name   = azurerm_resource_group.main.name
+#   location              = azurerm_resource_group.main.location
+#   subnet_id             = azurerm_subnet.keyvault_subnet.id
+#   private_endpoint_name = "keyvault"
+#   resource_id           = azurerm_key_vault.main.id
+#   subresource_names     = ["Vault"]
+#   tags                  = var.tags
+#   private_dns_zone_name = "privatelink.vaultcore.azure.net"
+#   jump_vnet_id          = azurerm_virtual_network.jump_vnet.id
+#   app_vnet_id           = azurerm_virtual_network.app_vnet.id
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,21 +2,21 @@ output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.main.name
-}
+# output "storage_account_name" {
+#   value = azurerm_storage_account.main.name
+# }
 
-output "key_vault_name" {
-  value = azurerm_key_vault.main.name
-}
+# output "key_vault_name" {
+#   value = azurerm_key_vault.main.name
+# }
 
-output "postgres_server_name" {
-  value = azurerm_postgresql_flexible_server.main.fqdn
-}
+# output "postgres_server_name" {
+#   value = azurerm_postgresql_flexible_server.main.fqdn
+# }
 
-output "redis_name" {
-  value = azurerm_managed_redis.main.name
-}
+# output "redis_name" {
+#   value = azurerm_managed_redis.main.name
+# }
 
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.main.name
@@ -26,6 +26,6 @@ output "app_vnet_name" {
   value = azurerm_virtual_network.app_vnet.name
 }
 
-output "mongo_cluster_name" {
-  value = azurerm_mongo_cluster.main.name
-}
+# output "mongo_cluster_name" {
+#   value = azurerm_mongo_cluster.main.name
+# }
